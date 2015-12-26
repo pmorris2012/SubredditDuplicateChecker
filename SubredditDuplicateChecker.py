7b02e342bfcd22cd21c4b4b37e28b60c484adda3
pmorris2012 <pmorris2012@fau.edu>
2015-12-25 20:11:35 -0500

    Blanket Exception Handling to prevent crash when internet connection is lost
    Wrapped main loop in a try statement, exceptions are ignored but the
    program continues looping.  prevents a crash when internet connection is
    lost.

--- SubredditDuplicateChecker.py
+++ SubredditDuplicateChecker.py
@@ -18,7 +18,7 @@ subreddit - the name of the subreddit to check (accepts multireddit syntax)
 interval - check for new posts every interval seconds
 num_posts - the number of posts to grab each check
 '''
-def run(user_agent, subreddit, interval = 15, num_posts = 5):
+def run(user_agent, subreddit, interval = 10, num_posts = 5):
     #dictionary of posts, key = post id, value = submission
     seen = {}
     # dictionary of hashes, key = hash, value = list of posts
@@ -36,49 +36,55 @@ def run(user_agent, subreddit, interval = 15, num_posts = 5):
     sub = r.get_subreddit(subreddit)
 
     while True:
-        #get new posts
-        new = sub.get_new(limit=num_posts)
-        for submission in new:
-            #if we've already seen this post, ignore it
-            if seen.has_key(submission.id):
-                continue
-            #if this post doens't link to imgur, ignore it
-            if "imgur.com/" not in submission.url:
-                continue
-            split = submission.url.split(".com/")
-            #if the post has a / after the .com/, it links to an album. ignore
-            if "/" in split[1]:
-                continue
+        print "trying"
+        try:
+            #get new posts
+            new = sub.get_new(limit=num_posts)
+            for submission in new:
+                #if we've already seen this post, ignore it
+                if seen.has_key(submission.id):
+                    continue
+                #if this post doens't link to imgur, ignore it
+                if "imgur.com/" not in submission.url:
+                    continue
+                split = submission.url.split(".com/")
+                #if the post has a / after the .com/, it links to an album. ignore
+                if "/" in split[1]:
+                    continue
 
-            #mark this post as seen - add it to the table
-            seen[submission.id] = submission
+                #mark this post as seen - add it to the table
+                seen[submission.id] = submission
 
-            #chop off a file extension after the . to get the id
-            imgur_id = split[1].split(".")[0]
+                #chop off a file extension after the . to get the id
+                split[1] = split[1].replace("?", ".")
+                imgur_id = split[1].split(".")[0]
 
-            #get the image from the url
-            res = get("https://i.imgur.com/" + imgur_id + ".jpg")
-            img = Image.open(StringIO(res.content))
+                #get the image from the url
+                res = get("https://i.imgur.com/" + imgur_id + ".jpg")
+                img = Image.open(StringIO(res.content))
 
-            #hash the image
-            hash = pHash(img, 64)
+                #hash the image
+                hash = pHash(img, 64)
 
-            #if this post is a duplicate, add it to the list for that hash
-            if hashes.has_key(hash):
-                print "{0} copied {1}!!!".format(submission.id, hashes[hash][0])
-                hashes[hash].append(submission.id)
-            else:
-                print "new post found: {0}".format(submission.id)
-                hashes[hash] = [submission.id]
+                #if this post is a duplicate, add it to the list for that hash
+                if hashes.has_key(hash):
+                    print "{0} copied {1}!!!".format(submission.id, hashes[hash][0])
+                    hashes[hash].append(submission.id)
+                else:
+                    print "new post found: {0}".format(submission.id)
+                    hashes[hash] = [submission.id]
 
-            #dump the updated seen and hashes objects to a file
-            with open("s_" + subreddit + ".txt", "w") as f:
-                f.write(dumps(seen))
-            with open("h_" + subreddit + ".txt", "w") as f:
-                f.write(dumps(hashes))
+                #dump the updated seen and hashes objects to a file
+                with open("s_" + subreddit + ".txt", "w") as f:
+                    f.write(dumps(seen))
+                with open("h_" + subreddit + ".txt", "w") as f:
+                    f.write(dumps(hashes))
+
+        except:
+            pass
 
         #wait the desired interval of time before checking again
         sleep(interval)
 
 if __name__ == "__main__":
-    run("duplicatechecker" + str(randint(0, 9999)), "pics", num_posts = 10)
+    run("duplicatechecker" + str(randint(0, 9999)), "pics+funny+me_irl", num_posts = 10)
