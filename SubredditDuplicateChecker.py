dfa3d012546ac53a31db1217b403fbad3d2961ec
Paul Morris <pmorris2012@fau.edu>
2016-01-05 11:04:54 -0500

    Added dictionaries for offenses/offenders

--- SubredditDuplicateChecker.py
+++ SubredditDuplicateChecker.py
@@ -3,7 +3,8 @@ from time import sleep
 from StringIO import StringIO
 from requests import get
 from pickle import loads, dumps
-from os.path import isfile
+from os.path import isfile, exists
+from os import makedirs
 from random import randint
 
 #non-standard modules - need to be installed
@@ -18,28 +19,39 @@ subreddit - the name of the subreddit to check (accepts multireddit syntax)
 interval - check for new posts every interval seconds
 num_posts - the number of posts to grab each check
 '''
-def run(user_agent, subreddit, interval = 60, num_posts = 20):
+def run(user_agent, sub, interval = 60, num_posts = 20):
     #dictionary of posts, key = post id, value = submission
     seen = {}
-    # dictionary of hashes, key = hash, value = list of posts
+    #dictionary of hashes, key = hash, value = list of posts
     hashes = {}
+    #dictionary of posts where an offense has occured
+    #key = post id, value = post id
+    offenses = {}
+    #dictionary of users who have offended, key = user, value = list of posts
+    offenders = {}
+
 
     #if a saved state exists, load it
-    if isfile("s_" + subreddit + ".txt") and isfile("h_" + subreddit + ".txt"):
-        with open("s_" + subreddit + ".txt") as f:
+    if isfile("save/s_" + sub + ".txt") and isfile("save/h_" + sub + ".txt")\
+    and isfile("save/o_" + sub + ".txt") and isfile("save/u_" + sub + ".txt"):
+        with open("save/s_" + sub + ".txt", "r") as f:
             seen = loads(f.read())
-        with open("h_" + subreddit + ".txt") as f:
+        with open("save/h_" + sub + ".txt", "r") as f:
             hashes = loads(f.read())
+        with open("save/o_" + sub + ".txt", "r") as f:
+            offenses = loads(f.read())
+        with open("save/u_" + sub + ".txt", "r") as f:
+            offenders = loads(f.read())
 
     #establish connection with reddit and get subreddit object
     r = Reddit(user_agent=user_agent)
-    sub = r.get_subreddit(subreddit)
+    subreddit = r.get_subreddit(sub)
 
     while True:
-        print "trying"
+        print "checking for posts"
         try:
             #get new posts
-            new = sub.get_new(limit=num_posts)
+            new = subreddit.get_new(limit=num_posts)
             for submission in new:
                 #if we've already seen this post, ignore it
                 if seen.has_key(submission.id):
@@ -68,25 +80,35 @@ def run(user_agent, subreddit, interval = 60, num_posts = 20):
 
                 #if this post is a duplicate, add it to the list for that hash
                 if hashes.has_key(hash):
-                    if submission.author == seen[hashes[hash][0]].author:
+                    orig = hashes[hash][0]
+                    post = submission.id
+                    if submission.author == seen[orig].author:
                         print "{0} posted duplicate posts {1} and {2}".format(
-                            submission.author, submission.id, hashes[hash][0])
+                            submission.author, post, orig)
                     else:
-                        print "{0} copied {1}!!!".format(
-                            submission.id, hashes[hash][0])
+                        print "{0} copied {1}!!!".format(post, orig)
+                        offenses[post] = orig
+                        offenders.setdefault(submission.author, []).append(post)
                     hashes[hash].append(submission.id)
                 else:
                     print "new post found: {0}".format(submission.id)
                     hashes[hash] = [submission.id]
 
+                #ensure the save directory exists
+                if not exists("save"):
+                    makedirs("save")
                 #dump the updated seen and hashes objects to a file
-                with open("s_" + subreddit + ".txt", "w") as f:
+                with open("save/s_" + sub + ".txt", "w") as f:
                     f.write(dumps(seen))
-                with open("h_" + subreddit + ".txt", "w") as f:
+                with open("save/h_" + sub + ".txt", "w") as f:
                     f.write(dumps(hashes))
+                with open("save/o_" + sub + ".txt", "w") as f:
+                    f.write(dumps(offenses))
+                with open("save/u_" + sub + ".txt", "w") as f:
+                    f.write(dumps(offenders))
 
-        except:
-            pass
+        except Exception as e:
+            print e.message
 
         #wait the desired interval of time before checking again
         sleep(interval)
